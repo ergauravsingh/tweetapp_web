d6604705442edb4a4f7b34685c11b12cbf41a6aa
ergauravpersonal <109891018+ergauravpersonal@users.noreply.github.com>
2022-08-31 22:01:57 +0530

    Add spinner

--- src/Component/Spinner.js
+++ src/Component/Spinner.js
@@ -3,9 +3,5 @@ import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
 export default function Spinner() {
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CircularProgress />
-    </Box>
-  );
+  return <CircularProgress className="twitter-spinner" />;
 }
