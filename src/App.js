92afd8acbaec16fa93684d0d074c5edbdc5397f8
ergauravpersonal <109891018+ergauravpersonal@users.noreply.github.com>
2022-08-31 22:10:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingView from "./Component/LandingView";
 import Home from "./Component/Home";
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 import { Container } from "@mui/material";
 
 function App() {
