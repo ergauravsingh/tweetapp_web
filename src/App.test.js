d62344498e49005984b4e1094026c1918a5e94b3
ergauravpersonal <109891018+ergauravpersonal@users.noreply.github.com>
2022-09-08 10:53:00 +0530

    Test case for login page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test if login page is rendered successfully", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    "See what’s happening in the world right now"
+  );
   expect(linkElement).toBeInTheDocument();
 });
