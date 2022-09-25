ca0d4d1d44d457aad5cdad2cbd3936d54556a882
ergauravpersonal <109891018+ergauravpersonal@users.noreply.github.com>
2022-09-25 21:38:10 +0530

    Update environmentConstants.js

--- src/environmentConstants.js
+++ src/environmentConstants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:9085/api/v1.0/tweets";
+export const BASE_URL =
+  "http://ec2-44-202-120-99.compute-1.amazonaws.com:9085/api/v1.0/tweets";
 export const BASE_URL_PROD = "";
